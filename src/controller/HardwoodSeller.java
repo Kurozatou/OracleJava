a7b712f94b325e22fe4d4700e10867fbd8fad11e
Elliot Rauchwerger <elliotrauchwerger@gmail.com>
2016-01-28 04:08:57 -0500

    Add initial prompt
    A prompt has been added in main so users can input file name.
    Additionally, started work on function to parse through input file.

--- src/controller/HardwoodSeller.java
+++ src/controller/HardwoodSeller.java
@@ -3,6 +3,10 @@
  */
 package controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * @author Esteban
  *
@@ -11,13 +15,25 @@ public class HardwoodSeller {
 
 	/**
 	 * @param args
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws FileNotFoundException {
+		// Create a new scanner object
+		Scanner s = new Scanner(System.in);
+		// display prompt
+		System.out.printf("Enter the name of your input file: ");
+		// read in file name
+		String filePath = s.next();
+		// read input using file path
+		readInputFile(filePath);
 	}
 	
-	public void readInputFile(String inputFilePath){
+	public static void readInputFile(String inputFilePath) throws FileNotFoundException{
+		// open the file
+		File file = new File(inputFilePath);
+		// scan through it
+		Scanner in = new Scanner(file);
+		// get first line of info
 		
 	}
 	
